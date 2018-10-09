25385cf32040ab61fd325c1b8077249135adb7fe
Andy811 <andygood811@gmail.com>
2018-10-09 16:21:20 +0800

    toast,alertdialog,positive button

--- app/src/main/java/com/andy/bmi/MainActivity.java
+++ app/src/main/java/com/andy/bmi/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
                         edHeight.setText("");
                         edWeight.setText("");
                     }
-                }).show();
+                })
+                .show();
     }
 }
