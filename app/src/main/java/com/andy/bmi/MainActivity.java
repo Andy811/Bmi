2f762c15a68b54ffd7bf2bbbcee18566210ed9ec
Andy811 <andygood811@gmail.com>
2018-10-23 16:26:36 +0800

    intent

--- app/src/main/java/com/andy/bmi/MainActivity.java
+++ app/src/main/java/com/andy/bmi/MainActivity.java
@@ -1,6 +1,9 @@
 package com.andy.bmi;
 
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,14 +13,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.time.Instant;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText edWeight;
     private EditText edHeight;
     private Button help;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+     // Log.d("MainActivity", "onCreate");
         setContentView(R.layout.activity_main);
         findViews();
     }
@@ -29,18 +36,18 @@ public class MainActivity extends AppCompatActivity {
         help.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               Log.d("MainActivity", "onClick: help" );
-               new AlertDialog.Builder(MainActivity.this)
-                       .setMessage(R.string.bmi_info)
-                       .setTitle("What is BMI?")
-                       .setPositiveButton("OK",null)
-                       .show();
+                Log.d("MainActivity", "onClick: help");
+                new AlertDialog.Builder(MainActivity.this)
+                        .setMessage(R.string.bmi_info)
+                        .setTitle("What is BMI?")
+                        .setPositiveButton("OK", null)
+                        .show();
 
             }
         });
     }
 
-    public void bmi(View view){
+    public void bmi(View view) {
         Log.d("MainActivity", "bmi");
 
         String w = edWeight.getText().toString();
@@ -50,18 +57,67 @@ public class MainActivity extends AppCompatActivity {
         float height = Float.parseFloat(h);
         float bmi = weight / (height * height);
         Log.d("MainActivity", bmi + "");
-        Toast.makeText(this,"your bmi ="+String.valueOf(bmi),Toast.LENGTH_LONG).show();
-        new AlertDialog.Builder(this)
-                .setMessage(getString(R.string.your_bmi_is)+bmi)
-                .setTitle(R.string.bmi)
-                .setPositiveButton("OK", null)
-                .setPositiveButton("Clear", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        edHeight.setText("");
-                        edWeight.setText("");
-                    }
-                })
-                .show();
+        Intent intent = new Intent(this, ResultActivity.class);
+       // intent.putExtra("BMI",bmi);
+        startActivity(intent.putExtra("BMI",bmi));
+
+//        Toast.makeText(this,getString(R.string.yourbmi)+String.valueOf(bmi),Toast.LENGTH_LONG).show();
+//        new AlertDialog.Builder(this)
+//                .setMessage(getString(R.string.your_bmi_is)+bmi)
+//                .setTitle(R.string.bmi)
+//                .setPositiveButton("OK", null)
+//                .setPositiveButton("Clear", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        edHeight.setText("");
+//                        edWeight.setText("");
+//                    }
+//                })
+//                .show();
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        Log.d("MainActivity", "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity", "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity", "onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActivity", "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MainActivity", "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MainActivity", "onResume");
+    }
+
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("MainActivity", "onRestart");
     }
 }
